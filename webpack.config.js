241add5d9014b7bbc05081c9f33b525f647a0e08
Vladimir Bataller <vbatalle@nttdata.com>
2022-09-22 16:58:05 +0200

    Plantillas HTML,  html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'my_app.js',
     path: path.resolve(__dirname, './dist')
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "index.html" })]
 }
+
 
